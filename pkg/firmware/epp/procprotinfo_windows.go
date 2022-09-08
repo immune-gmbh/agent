d7a3719ea7acf8ece8559d84d17a1c478a051267
Hans-Gert Dahmen <hans-gert.dahmen@immu.ne>
2022-09-08 17:07:36 +0200

    epp: list windows ELAM drivers and consolidate EPP related funcs into pkg

--- pkg/firmware/epp/procprotinfo_windows.go
+++ pkg/firmware/epp/procprotinfo_windows.go
@@ -1,4 +1,4 @@
-package osinfo
+package epp
 
 import (
 	"errors"
@@ -6,6 +6,11 @@ import (
 	"unsafe"
 
 	"golang.org/x/sys/windows"
+)
+
+const (
+	queryLimitedInformation      = 0x00001000
+	processProtectionInformation = 0x0000003D
 )
 
 type PsProtectedType uint32
@@ -95,7 +100,7 @@ func queryInformationProcess(procHandle windows.Handle, processInformationClass 
 
 func queryFullProcessImageName(procHandle windows.Handle) (string, error) {
 	for i := 2048; i <= 32768; i = i << 1 {
-		exeNameU16 := make([]uint16, 2048)
+		exeNameU16 := make([]uint16, i)
 		exeNameSz := uint32(len(exeNameU16))
 		err := windows.QueryFullProcessImageName(procHandle, 0, &exeNameU16[0], &exeNameSz)
 		if err != nil {
@@ -112,33 +117,78 @@ func queryFullProcessImageName(procHandle windows.Handle) (string, error) {
 	return "", errors.New("buffer too small")
 }
 
-func QueryProcessNameProtectionLvl(processId uint32) (string, PsProtectedType, PsProtectedSigner, error) {
-	const queryLimitedInformation = 0x00001000
-	const processProtectionInformation = 0x0000003D
-
-	procHandle, err := windows.OpenProcess(queryLimitedInformation, false, processId)
-	if err != nil {
-		return "", 0, 0, err
-	}
-
+func QueryProcessProtectionLvl(procHandle windows.Handle) (PsProtectedType, PsProtectedSigner, error) {
 	buf := make([]byte, 1)
 	retLen, err := queryInformationProcess(procHandle, processProtectionInformation, buf)
 	if err != nil {
-		return "", 0, 0, err
+		return 0, 0, err
 	}
 	if retLen != 1 {
-		return "", 0, 0, errors.New("wrong return size")
+		return 0, 0, errors.New("wrong return size")
 	}
 
-	exePath, err := queryFullProcessImageName(procHandle)
+	return PsProtectedType(buf[0] & 0x07), PsProtectedSigner((buf[0] & 0xf0) >> 4), nil
+}
+
+func enumProcesses() ([]uint32, error) {
+	for i := 1024; i <= 32768; i = i << 1 {
+		var bytesReturned uint32
+		processIds := make([]uint32, i)
+		err := windows.EnumProcesses(processIds, &bytesReturned)
+		if err != nil {
+			return nil, err
+		}
+
+		// if we got less data than our buffer is long then probably we have all processes
+		if i < int(bytesReturned/4) {
+			return processIds[:(bytesReturned / 4)], nil
+		}
+	}
+
+	return nil, errors.New("buffer too small")
+}
+
+func getFullImagePathOnlyIfPPLProc(processId uint32) (string, error) {
+	procHandle, err := windows.OpenProcess(queryLimitedInformation, false, processId)
 	if err != nil {
-		return "", 0, 0, err
+		return "", err
 	}
+	defer windows.CloseHandle(procHandle)
 
-	err = windows.CloseHandle(procHandle)
+	lvl, sig, err := QueryProcessProtectionLvl(procHandle)
 	if err != nil {
-		return "", 0, 0, err
+		return "", err
 	}
 
-	return exePath, PsProtectedType(buf[0] & 0x07), PsProtectedSigner((buf[0] & 0xf0) >> 4), nil
+	if lvl == TypeProtectedLight && sig == SignerAntimalware {
+		name, err := queryFullProcessImageName(procHandle)
+		return name, err
+	}
+
+	return "", nil
+}
+
+// ListPPLProcessImagePaths lists image paths of processes with process protection light enabled
+func ListPPLProcessImagePaths() ([]string, error) {
+	procIds, err := enumProcesses()
+	if err != nil {
+		return nil, err
+	}
+
+	var list []string
+	var lastErr error
+	for _, id := range procIds {
+		name, err := getFullImagePathOnlyIfPPLProc(id)
+		if err == nil {
+			list = append(list, name)
+		} else {
+			lastErr = err
+		}
+	}
+
+	if len(list) == 0 {
+		return list, lastErr
+	}
+
+	return list, nil
 }
