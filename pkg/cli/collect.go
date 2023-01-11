257c16604cbfcea9e8ada88c5b5e94fc3cc8516b
Hans-Gert Dahmen <hans-gert.dahmen@immu.ne>
2023-01-11 13:58:53 +0100

    cli: put each subcommand into its own file

---
+++ pkg/cli/collect.go
@@ -0,0 +1,26 @@
+package cli
+
+import (
+	"context"
+
+	"github.com/immune-gmbh/agent/v3/pkg/api"
+	"github.com/immune-gmbh/agent/v3/pkg/attestation"
+	"github.com/immune-gmbh/agent/v3/pkg/tui"
+)
+
+type collectCmd struct {
+}
+
+func (collect *collectCmd) Run(glob *globalOptions) error {
+	ctx := context.Background()
+	cfg := api.Configuration{}
+
+	err := attestation.Collect(ctx, &cfg)
+	if err != nil {
+		tui.SetUIState(tui.StAttestationFailed)
+		return err
+	}
+
+	tui.SetUIState(tui.StAttestationSuccess)
+	return nil
+}
---
+++ pkg/cli/enroll.go
@@ -0,0 +1,76 @@
+package cli
+
+import (
+	"context"
+	"os"
+
+	"github.com/immune-gmbh/agent/v3/pkg/attestation"
+	"github.com/immune-gmbh/agent/v3/pkg/state"
+	"github.com/immune-gmbh/agent/v3/pkg/tcg"
+	"github.com/immune-gmbh/agent/v3/pkg/tui"
+	"github.com/sirupsen/logrus"
+)
+
+type enrollCmd struct {
+	NoAttest bool   `help:"Don't attest after successful enrollment" default:"false"`
+	Token    string `arg:"" required:"" name:"token" help:"Enrollment authentication token"`
+	Name     string `arg:"" optional:"" name:"name hint" help:"Name to assign to the device. May get suffixed by a counter if already taken. Defaults to the hostname."`
+	TPM      string `name:"tpm" default:"${tpm_default_path}" help:"TPM device: device path (${tpm_default_path}) or mssim, sgx, swtpm/net url (mssim://localhost, sgx://localhost, net://localhost:1234) or 'dummy' for dummy TPM"`
+	DummyTPM bool   `name:"notpm" help:"Force using insecure dummy TPM if this device has no real TPM" default:"false"`
+}
+
+func (enroll *enrollCmd) Run(glob *globalOptions) error {
+	ctx := context.Background()
+
+	// store used TPM in state, use dummy TPM only if forced
+	if enroll.DummyTPM {
+		glob.State.TPM = state.DummyTPMIdentifier
+	} else {
+		glob.State.TPM = enroll.TPM
+	}
+
+	if err := openTPM(glob); err != nil {
+		if glob.State.TPM != state.DummyTPMIdentifier {
+			tui.SetUIState(tui.StSelectTAFailed)
+		}
+		return err
+	}
+	tui.SetUIState(tui.StSelectTASuccess)
+
+	// when server is set on cmdline during enroll store it in state
+	// so OS startup scripts can attest without needing to know the server URL
+	if cli.Server != nil {
+		glob.State.ServerURL = cli.Server
+	}
+
+	if err := attestation.Enroll(ctx, &glob.Client, enroll.Token, glob.EndorsementAuth, defaultNameHint, glob.Anchor, glob.State); err != nil {
+		tui.SetUIState(tui.StEnrollFailed)
+		return err
+	}
+
+	// incorporate dummy TPM state
+	if stub, ok := glob.Anchor.(*tcg.SoftwareAnchor); ok {
+		if st, err := stub.Store(); err != nil {
+			logrus.Debugf("SoftwareAnchor.Store: %s", err)
+			logrus.Errorf("Failed to save stub TPM state to disk")
+		} else {
+			glob.State.StubState = st
+		}
+	}
+
+	// save the new state to disk
+	if err := glob.State.Store(glob.StatePath); err != nil {
+		logrus.Debugf("Store(%s): %s", glob.StatePath, err)
+		logrus.Errorf("Failed to save activated keys to disk")
+		return err
+	}
+
+	tui.SetUIState(tui.StEnrollSuccess)
+	logrus.Infof("Device enrolled")
+	if enroll.NoAttest {
+		logrus.Infof("You can now attest with \"%s attest\"", os.Args[0])
+		return nil
+	}
+
+	return doAttest(glob, ctx, "", false)
+}
