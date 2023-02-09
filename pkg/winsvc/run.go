8cd97fb62d9af76e35ff3173e76f0f5b4829a429
Hans-Gert Dahmen <hans-gert.dahmen@immu.ne>
2023-02-09 16:04:24 +0100

    winsvc: expose windows service via IPC

--- pkg/winsvc/run.go
+++ pkg/winsvc/run.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/immune-gmbh/agent/v3/pkg/core"
+	"github.com/immune-gmbh/agent/v3/pkg/ipc"
 	"github.com/immune-gmbh/agent/v3/pkg/state"
 	"github.com/immune-gmbh/agent/v3/pkg/util"
 	"github.com/rs/zerolog"
@@ -21,6 +22,8 @@ const (
 	SVC_DESC              = "immune Guard Agent Service"
 	defaultAttestInterval = time.Hour
 )
+
+var defaultAttestArgs = ipc.CmdArgsAttest{DryRun: false}
 
 type Exponential struct {
 	Min        time.Duration
@@ -47,8 +50,10 @@ func (e *Exponential) Increase() time.Duration {
 }
 
 type agentService struct {
-	backoff *Exponential
-	core    *core.AttestationClient
+	backoff          *Exponential
+	agent            *ipc.SharedAgentResource
+	cancelPipeServer context.CancelFunc
+	svcReleaseId     *string
 }
 
 func (m *agentService) Execute(args []string, r <-chan svc.ChangeRequest, changes chan<- svc.Status) (ssec bool, errno uint32) {
@@ -57,7 +62,7 @@ func (m *agentService) Execute(args []string, r <-chan svc.ChangeRequest, change
 	scheduleInterval := time.Millisecond
 	m.backoff = &Exponential{Min: time.Minute, Max: defaultAttestInterval}
 	changes <- svc.Status{State: svc.Running, Accepts: cmdsAccepted}
-	log.Info().Msgf("immune Guard agent service %s (%s) started", *m.core.ReleaseId, runtime.GOARCH)
+	log.Info().Msgf("immune Guard agent service %s (%s) started", *m.svcReleaseId, runtime.GOARCH)
 
 	defer func() {
 		if err := recover(); err != nil {
@@ -77,6 +82,7 @@ loop:
 				changes <- c.CurrentStatus
 			case svc.Stop, svc.Shutdown:
 				log.Info().Msg("stopping")
+				m.cancelPipeServer()
 				break loop
 			default:
 				log.Error().Msgf("unexpected control request #%d", c)
@@ -86,12 +92,27 @@ loop:
 	return
 }
 
+// XXX consider running agent ops in a goroutine to not block the winsvc messaging thread
+// -> test what happens if I query status or stop service when long attest is running
 func (m *agentService) runAttest() time.Duration {
-	ctx := context.Background()
+	status := m.agent.Status()
+	if !status.Enrolled {
+		return defaultAttestInterval
+	}
 
-	// run attest and retry with exponential backoff in case of error
-	if _, err := m.core.Attest(ctx, false); err != nil {
+	// if the last operation is recent then reschedule accordingly
+	if status.LastRun != nil && status.LastOperation != "" {
+		d := time.Since(*status.LastRun)
+		if d < defaultAttestInterval {
+			return defaultAttestInterval - d
+		}
+	}
+
+	// run attest and retry with exponential backoff in case of error or non exclusive access
+	if exclusive, err := m.agent.TryAttest(context.Background(), nil, &defaultAttestArgs); err != nil {
 		core.LogAttestErrors(&log.Logger, err)
+		return m.backoff.Increase()
+	} else if !exclusive {
 		return m.backoff.Increase()
 	}
 	m.backoff.Reset()
@@ -108,14 +129,16 @@ func RunService() int {
 	defer elog.Close()
 
 	// configure logging and redirect global logger output to event log
-	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	zerolog.SetGlobalLevel(zerolog.TraceLevel)
+	log.Logger = log.Level(zerolog.InfoLevel)
 	ew := &eventLogWriter{
 		elog: elog,
 		cw: &zerolog.ConsoleWriter{
 			NoColor: true, PartsExclude: []string{zerolog.LevelFieldName, zerolog.TimestampFieldName},
 		},
 	}
-	log.Logger = log.Logger.Output(zerolog.SyncWriter(ew))
+	sew := zerolog.SyncWriter(ew)
+	log.Logger = log.Logger.Output(sew)
 
 	// bail out if not root
 	root, err := util.IsRoot()
@@ -128,18 +151,24 @@ func RunService() int {
 	}
 
 	// init agent core
-	agentCore := core.NewCore()
-	if err := agentCore.Init(state.DefaultStateDir(), &log.Logger); err != nil {
+	agent := core.NewCore()
+	if err := agent.Init(state.DefaultStateDir(), &log.Logger); err != nil {
 		core.LogInitErrors(&log.Logger, err)
 		return 1
 	}
 
-	if !agentCore.State.IsEnrolled() {
-		log.Error().Msg("No previous state found, please enroll first.")
+	// start a shared agent service on a named pipe
+	sharedAgent := ipc.NewSharedAgent(agent)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	err = ipc.StartNamedPipe(ctx, sew, sharedAgent, agent.ReleaseId)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to start named pipe")
 		return 1
 	}
 
-	err = svc.Run(SVC_NAME, &agentService{core: agentCore})
+	// when all went well proceed to execute as a windows service
+	err = svc.Run(SVC_NAME, &agentService{agent: sharedAgent, cancelPipeServer: cancel, svcReleaseId: agent.ReleaseId})
 	if err != nil {
 		log.Error().Msgf("%s service failed: %v", SVC_NAME, err)
 		return 1
